78f69de17269ab72b03afd9574bed724e2fefb84
Brendan Graetz <bguiz@users.noreply.github.com>
2024-07-09 13:03:12 +0800

    feat: remove prompt to input operator ID when not in .env file
    - no longer need to do so, as the 01-dotenv-app.js script now populates this automatically for both private key and seed phrase options
    
    Signed-off-by: Brendan Graetz <bguiz@users.noreply.github.com>

--- init/02-dotenv-rpcrelay.js
+++ init/02-dotenv-rpcrelay.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 
-const readline = require('node:readline/promises');
 const fs = require('fs/promises');
 const path = require('path');
-const { stdin, stdout } = require('node:process');
 const dotenv = require('dotenv');
 
 const DEFAULT_VALUES = {
@@ -16,28 +14,15 @@ async function initDotEnvForRpcRelay() {
     dotenv.config({ path: DEFAULT_VALUES.appDotEnvFilePath });
     const {
         OPERATOR_ACCOUNT_PRIVATE_KEY,
-        OPERATOR_ACCOUNT_EVM_ADDRESS,
         OPERATOR_ACCOUNT_ID,
     } = process.env;
 
     let operatorId = OPERATOR_ACCOUNT_ID;
-    let operatorEvmAddress = OPERATOR_ACCOUNT_EVM_ADDRESS;
     let operatorKey = OPERATOR_ACCOUNT_PRIVATE_KEY;
 
-    const rlPrompt = readline.createInterface({
-        input: stdin,
-        output: stdout,
-    });
-
-    if (!operatorId && !operatorEvmAddress) {
-        console.log('Must define operator ID or operator EVM address');
-        rlPrompt.close();
+    if (!operatorId) {
+        console.error('Must define operator ID');
         return;
-    } else if (!operatorId) {
-        console.log(`Please enter the operator account ID which corresponds to ${operatorEvmAddress}`);
-        console.log('If this account has not yet been created or funded, you may do so via https://faucet.hedera.com');
-        const inputOperatorId = await rlPrompt.question('> ');
-        operatorId = inputOperatorId;
     }
 
 const dotEnvText =
@@ -52,8 +37,6 @@ MIRROR_NODE_URL=https://testnet.mirrornode.hedera.com/
     await fs.writeFile(fileName, dotEnvText);
 
     console.log('OK, wrote .rpcrelay.env file');
-
-    rlPrompt.close();
 }
 
 initDotEnvForRpcRelay();
