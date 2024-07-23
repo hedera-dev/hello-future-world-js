74a0dbda50f5b489d20a95e35e559e118d4ab6c1
Brendan Graetz <bguiz@users.noreply.github.com>
2024-07-23 12:37:26 +0800

    refactor: HTS FT HW seq to use new logger
    Signed-off-by: Brendan Graetz <bguiz@users.noreply.github.com>

--- hts-ft/script-hts-ft.js
+++ hts-ft/script-hts-ft.js
@@ -9,19 +9,21 @@ import {
 } from '@hashgraph/sdk';
 import dotenv from 'dotenv';
 import {
-    blueLog,
-    metricsTrackOnHcs,
+    createLogger,
 } from '../util/util.js';
 
-const hfwId = 'HFW-HTS';
-
+const logger = await createLogger({
+    scriptId: 'htsFt',
+    scriptCategory: 'task',
+});
 let client;
 
 async function scriptHtsFungibleToken() {
-    metricsTrackOnHcs('scriptHtsFungibleToken', 'run');
+    logger.logStart('Hello Future World - HTS Fungible Token - start');
 
     // Read in environment variables from `.env` file in parent directory
     dotenv.config({ path: '../.env' });
+    logger.log('Read .env file');
 
     // Initialise the operator account
     const yourName = process.env.YOUR_NAME;
@@ -33,18 +35,20 @@ async function scriptHtsFungibleToken() {
     const operatorId = AccountId.fromString(operatorIdStr);
     const operatorKey = PrivateKey.fromStringECDSA(operatorKeyStr);
     client = Client.forTestnet().setOperator(operatorId, operatorKey);
+    logger.log('Using your name as:', yourName);
+    logger.log('Using account:', operatorIdStr);
 
     // NOTE: Create a HTS token
     // Step (1) in the accompanying tutorial
-    blueLog('Creating new HTS token');
+    logger.logSection('Creating new HTS token');
     const tokenCreateTx = await new TokenCreateTransaction()
-        .setTransactionMemo(hfwId)
-        .setTokenMemo(`${hfwId} token by ${yourName}`)
+        .setTransactionMemo(logger.scriptId)
+        .setTokenMemo(`${logger.scriptId} token by ${yourName}`)
         // HTS `TokenType.FungibleCommon` behaves similarly to ERC20
         .setTokenType(TokenType.FungibleCommon)
         // Configure token options: name, symbol, decimals, initial supply
         .setTokenName(`${yourName} coin`)
-        .setTokenSymbol(hfwId)
+        .setTokenSymbol(logger.scriptId)
         .setDecimals(2)
         .setInitialSupply(1_000_000)
         // Configure token access permissions: treasury account, admin, freezing
@@ -56,7 +60,7 @@ async function scriptHtsFungibleToken() {
 
     // Get the transaction ID of the transaction. The SDK automatically generates and assigns a transaction ID when the transaction is created
     const tokenCreateTxId = tokenCreateTx.transactionId;
-    console.log('The token create transaction ID: ',
+    logger.log('The token create transaction ID: ',
         tokenCreateTxId.toString());
 
     // Sign the transaction with the account key that will be paying for this transaction
@@ -70,8 +74,7 @@ async function scriptHtsFungibleToken() {
 
     // Get the token ID
     const tokenId = tokenCreateTxReceipt.tokenId;
-    console.log('tokenId:', tokenId.toString());
-    console.log('');
+    logger.log('tokenId:', tokenId.toString());
 
     client.close();
 
@@ -80,20 +83,19 @@ async function scriptHtsFungibleToken() {
     // This is a manual step, the code below only outputs the URL to visit
 
     // View your token on HashScan
-    blueLog('View the token on HashScan');
+    logger.logSection('View the token on HashScan');
     const tokenVerifyHashscanUrl = `https://hashscan.io/testnet/token/${tokenId.toString()}`;
-    console.log('Paste URL in browser:', tokenVerifyHashscanUrl);
-    console.log('');
+    logger.log('Paste URL in browser:', tokenVerifyHashscanUrl);
 
     // Wait for 6s for record files (blocks) to propagate to mirror nodes
     await new Promise((resolve) => setTimeout(resolve, 6_000));
 
     // NOTE: Verify token using Mirror Node API
     // Step (3) in the accompanying tutorial
-    blueLog('Get token data from the Hedera Mirror Node');
+    logger.logSection('Get token data from the Hedera Mirror Node');
     const tokenVerifyMirrorNodeApiUrl =
         `https://testnet.mirrornode.hedera.com/api/v1/tokens/${tokenId.toString()}`;
-    console.log(
+    logger.log(
         'The token Hedera Mirror Node API URL:\n',
         tokenVerifyMirrorNodeApiUrl
     );
@@ -101,19 +103,15 @@ async function scriptHtsFungibleToken() {
     const tokenVerifyJson = await tokenVerifyFetch.json();
     const tokenVerifyName =
         tokenVerifyJson?.name;
-    console.log('The name of this token:', tokenVerifyName);
+    logger.log('The name of this token:', tokenVerifyName);
     const tokenVerifyTotalSupply =
         tokenVerifyJson?.total_supply;
-    console.log('The total supply of this token:', tokenVerifyTotalSupply);
-    console.log('');
+    logger.log('The total supply of this token:', tokenVerifyTotalSupply);
 
-    metricsTrackOnHcs('scriptHtsFungibleToken', 'complete');
+    logger.logComplete('Hello Future World - HTS Fungible Token - complete');
 }
 
 scriptHtsFungibleToken().catch((ex) => {
-    if (client) {
-        client.close();
-    }
-    console.error(ex);
-    metricsTrackOnHcs('scriptHtsFungibleToken', 'error');
+    client && client.close();
+    logger ? logger.logError(ex) : console.error(ex);
 });
