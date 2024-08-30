a00d2026b24483d479f0c2962ca2a4e05b98b3f4
krystal <krystal@swirldslabs.com>
2024-08-29 19:34:30 -0700

    initial commit

--- transfer/script-transfer-hbar.js
+++ transfer/script-transfer-hbar.js
@@ -1,146 +1,162 @@
 #!/usr/bin/env node
 
 import {
-    Client,
-    PrivateKey,
-    AccountId,
-    TransferTransaction,
-    Hbar,
-    HbarUnit,
-    AccountBalanceQuery,
-} from '@hashgraph/sdk';
-import dotenv from 'dotenv';
+  Client,
+  PrivateKey,
+  AccountId,
+  TransferTransaction,
+  Hbar,
+  HbarUnit,
+  AccountBalanceQuery,
+} from "@hashgraph/sdk";
+import dotenv from "dotenv";
 import {
-    convertTransactionIdForMirrorNodeApi,
-    createLogger,
-} from '../util/util.js';
+  convertTransactionIdForMirrorNodeApi,
+  createLogger,
+} from "../util/util.js";
 
 const logger = await createLogger({
-    scriptId: 'transferHbar',
-    scriptCategory: 'task',
+  scriptId: "transferHbar",
+  scriptCategory: "task",
 });
 let client;
 
 async function scriptTransferHbar() {
-    logger.logStart('Hello Future World - Transfer Hbar - start');
+  logger.logStart("Hello Future World - Transfer Hbar - start");
 
-    // Read in environment variables from `.env` file in parent directory
-    dotenv.config({ path: '../.env' });
-    logger.log('Read .env file');
+  // Read in environment variables from `.env` file in parent directory
+  dotenv.config({ path: "../.env" });
+  logger.log("Read .env file");
 
-    // Initialise the operator account
-    const operatorIdStr = process.env.OPERATOR_ACCOUNT_ID;
-    const operatorKeyStr = process.env.OPERATOR_ACCOUNT_PRIVATE_KEY;
+  // Initialize the operator account
+  const operatorIdStr = process.env.OPERATOR_ACCOUNT_ID;
+  const operatorKeyStr = process.env.OPERATOR_ACCOUNT_PRIVATE_KEY;
 
-    if (!operatorIdStr || !operatorKeyStr) {
-        throw new Error('Must set YOUR_NAME, OPERATOR_ACCOUNT_ID, OPERATOR_ACCOUNT_PRIVATE_KEY');
-    }
-    const operatorId = AccountId.fromString(operatorIdStr);
-    const operatorKey = PrivateKey.fromStringECDSA(operatorKeyStr);
-    logger.log('Using account:', operatorIdStr);
-
-    // The client operator ID and key is the account that will be automatically set to pay for the transaction fees for each transaction
-    client = Client.forTestnet().setOperator(operatorId, operatorKey);
-
-    // NOTE: Transfer HBAR using TransferTransaction
-    // Step (1) in the accompanying tutorial
-    await logger.logSection(
-        'Creating, signing, and submitting the transfer transaction');
-
-    const transferTx = await new TransferTransaction()
-        .setTransactionMemo(`Hello Future World transfer - ${logger.version}`)
-        // Debit  3 HBAR from the operator account (sender)
-        .addHbarTransfer(operatorId, new Hbar(-3, HbarUnit.Hbar))
-        // Credit 1 HBAR to account 0.0.200 (1st recipient)
-        .addHbarTransfer('0.0.200', new Hbar(1, HbarUnit.Hbar))
-        // Credit 2 HBAR to account 0.0.201 (2nd recipient)
-        .addHbarTransfer('0.0.201', new Hbar(2, HbarUnit.Hbar))
-        // Freeze the transaction to prepare for signing
-        .freezeWith(client);
-
-    // Get the transaction ID for the transfer transaction
-    const transferTxId = transferTx.transactionId;
-    logger.log('The transfer transaction ID:', transferTxId.toString());
-
-    // Sign the transaction with the account that is being debited (operator account) and the transaction fee payer account (operator account)
-    // Since the account that is being debited and the account that is paying for the transaction are the same, only one account's signature is required
-const transferTxSigned = await transferTx.sign(operatorKey);
-
-    //Submit the transaction to the Hedera Testnet
-    const transferTxSubmitted = await transferTxSigned.execute(client);
-
-    //Get the transfer transaction receipt
-    const transferTxReceipt = await transferTxSubmitted.getReceipt(client);
-    const transactionStatus = transferTxReceipt.status;
-    logger.log(
-        'The transfer transaction status is:',
-        transactionStatus.toString(),
+  if (!operatorIdStr || !operatorKeyStr) {
+    throw new Error(
+      "Must set YOUR_NAME, OPERATOR_ACCOUNT_ID, OPERATOR_ACCOUNT_PRIVATE_KEY"
     );
+  }
+  const operatorId = AccountId.fromString(operatorIdStr);
+  const operatorKey = PrivateKey.fromStringECDSA(operatorKeyStr);
+  logger.log("Using account:", operatorIdStr);
 
-    // NOTE: Query HBAR balance using AccountBalanceQuery
-    // Step (2) in the accompanying tutorial
-    const newAccountBalance = new AccountBalanceQuery()
-        .setAccountId('0.0.1')
-        .execute(client);
-    const newHbarBalance = (await newAccountBalance).hbars;
-    logger.log('The new account balance after the transfer:', newHbarBalance.toString());
+  // The client operator ID and key is the account that will be automatically set to pay for the transaction fees for each transaction
+  client = Client.forTestnet().setOperator(operatorId, operatorKey);
 
-    client.close();
+  //Set the default maximum transaction fee (in Hbar)
+  client.setDefaultMaxTransactionFee(new Hbar(100));
+  //Set the maximum payment for queries (in Hbar)
+  client.setDefaultMaxQueryPayment(new Hbar(50));
 
-    // View the transaction in HashScan
-    await logger.logSection('View the transfer transaction transaction in HashScan');
-    const transferTxVerifyHashscanUrl = `https://hashscan.io/testnet/transaction/${transferTxId}`;
-    logger.log(
-        'Copy and paste this URL in your browser:\n',
-        ...logger.applyAnsi('URL', transferTxVerifyHashscanUrl),
-    );
+  // NOTE: Transfer HBAR using TransferTransaction
+  await logger.logSection(
+    "Creating, signing, and submitting the transfer transaction"
+  );
 
-    await logger.logSection('Get transfer transaction data from the Hedera Mirror Node');
+  const transferTx = await new TransferTransaction()
+    .setTransactionMemo(`Hello Future World transfer - ${logger.version}`)
+    // Debit  3 HBAR from the operator account (sender)
+    .addHbarTransfer(operatorId, new Hbar(-3, HbarUnit.Hbar))
+    // Credit 1 HBAR to account 0.0.200 (1st recipient)
+    .addHbarTransfer("0.0.200", new Hbar(1, HbarUnit.Hbar))
+    // Credit 2 HBAR to account 0.0.201 (2nd recipient)
+    .addHbarTransfer("0.0.201", new Hbar(2, HbarUnit.Hbar))
+    // Freeze the transaction to prepare for signing
+    .freezeWith(client);
 
-    // Wait for 6s for record files (blocks) to propagate to mirror nodes
-    await new Promise((resolve) => setTimeout(resolve, 6_000));
+  // Get the transaction ID for the transfer transaction
+  const transferTxId = transferTx.transactionId;
+  logger.log("The transfer transaction ID:", transferTxId.toString());
 
-    // The transfer transaction mirror node API request
-    const transferTxIdMirrorNodeFormat = convertTransactionIdForMirrorNodeApi(transferTxId);
-    const transferTxVerifyMirrorNodeApiUrl = `https://testnet.mirrornode.hedera.com/api/v1/transactions/${transferTxIdMirrorNodeFormat}?nonce=0`;
-    logger.log(
-        'The transfer transaction Hedera Mirror Node API URL:\n',
-        ...logger.applyAnsi('URL', transferTxVerifyMirrorNodeApiUrl),
-    );
+  // Sign the transaction with the account that is being debited (operator account) and the transaction fee payer account (operator account)
+  // Since the account that is being debited and the account that is paying for the transaction are the same, only one account's signature is required
+  const transferTxSigned = await transferTx.sign(operatorKey);
 
-    // The transfer transaction assessed transaction fee, debits, and credits in HBAR
-    const transferFetch = await fetch(transferTxVerifyMirrorNodeApiUrl);
-    const transferJson = await transferFetch.json();
-    const transferJsonAccountTransfers = transferJson?.transactions[0]?.transfers;
-    // console.log(transferJsonAccountTransfers);
-    const transferJsonAccountTransfersFinalAmounts = transferJsonAccountTransfers
-        ?.filter((entry) => {
-            // Discard all entries whose values are less than 0.1 hbars
-            // as these are network fees
-            return (Math.abs(entry.amount) >= 100_000_00);
-        })
-        ?.sort((entryA, entryB) => {
-            return entryA.amount - entryB.amount;
-        })
-        ?.map((entry) => {
-            return {
-                'Account ID': entry.account,
-                'Amount': Hbar.from(entry.amount, HbarUnit.Tinybar).toString(HbarUnit.Hbar),
-            };
-        });
-    logger.log(
-        'The debit, credit, and transaction fee amounts of the transfer transaction:',
-      );
-    if (typeof console.table === 'function') {
-        console.table(transferJsonAccountTransfersFinalAmounts);
-    } else {
-        console.log(transferJsonAccountTransfersFinalAmounts);
-    }
+  //Submit the transaction to the Hedera Testnet
+  const transferTxSubmitted = await transferTxSigned.execute(client);
 
-    logger.logComplete('Hello Future World - Transfer Hbar - complete');
+  //Get the transfer transaction receipt
+  const transferTxReceipt = await transferTxSubmitted.getReceipt(client);
+  const transactionStatus = transferTxReceipt.status;
+  logger.log(
+    "The transfer transaction status is:",
+    transactionStatus.toString()
+  );
+
+  // NOTE: Query HBAR balance using AccountBalanceQuery
+  const newAccountBalance = new AccountBalanceQuery()
+    .setAccountId("0.0.1")
+    .execute(client);
+  const newHbarBalance = (await newAccountBalance).hbars;
+  logger.log(
+    "The new account balance after the transfer:",
+    newHbarBalance.toString()
+  );
+
+  client.close();
+
+  // View the transaction in HashScan
+  await logger.logSection(
+    "View the transfer transaction transaction in HashScan"
+  );
+  const transferTxVerifyHashscanUrl = `https://hashscan.io/testnet/transaction/${transferTxId}`;
+  logger.log(
+    "Copy and paste this URL in your browser:\n",
+    ...logger.applyAnsi("URL", transferTxVerifyHashscanUrl)
+  );
+
+  await logger.logSection(
+    "Get transfer transaction data from the Hedera Mirror Node"
+  );
+
+  // Wait for 6s for record files (blocks) to propagate to mirror nodes
+  await new Promise((resolve) => setTimeout(resolve, 6_000));
+
+  // The transfer transaction mirror node API request
+  const transferTxIdMirrorNodeFormat =
+    convertTransactionIdForMirrorNodeApi(transferTxId);
+  const transferTxVerifyMirrorNodeApiUrl = `https://testnet.mirrornode.hedera.com/api/v1/transactions/${transferTxIdMirrorNodeFormat}?nonce=0`;
+  logger.log(
+    "The transfer transaction Hedera Mirror Node API URL:\n",
+    ...logger.applyAnsi("URL", transferTxVerifyMirrorNodeApiUrl)
+  );
+
+  // The transfer transaction assessed transaction fee, debits, and credits in HBAR
+  const transferFetch = await fetch(transferTxVerifyMirrorNodeApiUrl);
+  const transferJson = await transferFetch.json();
+  const transferJsonAccountTransfers = transferJson?.transactions[0]?.transfers;
+  // console.log(transferJsonAccountTransfers);
+  const transferJsonAccountTransfersFinalAmounts = transferJsonAccountTransfers
+    ?.filter((entry) => {
+      // Discard all entries whose values are less than 0.1 hbars
+      // as these are network fees
+      return Math.abs(entry.amount) >= 100_000_00;
+    })
+    ?.sort((entryA, entryB) => {
+      return entryA.amount - entryB.amount;
+    })
+    ?.map((entry) => {
+      return {
+        "Account ID": entry.account,
+        Amount: Hbar.from(entry.amount, HbarUnit.Tinybar).toString(
+          HbarUnit.Hbar
+        ),
+      };
+    });
+  logger.log(
+    "The debit, credit, and transaction fee amounts of the transfer transaction:"
+  );
+  if (typeof console.table === "function") {
+    console.table(transferJsonAccountTransfersFinalAmounts);
+  } else {
+    console.log(transferJsonAccountTransfersFinalAmounts);
+  }
+
+  logger.logComplete("Hello Future World - Transfer Hbar - complete");
 }
 
 scriptTransferHbar().catch((ex) => {
-    client && client.close();
-    logger ? logger.logError(ex) : console.error(ex);
+  client && client.close();
+  logger ? logger.logError(ex) : console.error(ex);
 });
--- util/01-dotenv-app.js
+++ util/01-dotenv-app.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-const readline = require('node:readline/promises');
-const fs = require('fs/promises');
-const path = require('path');
-const { stdin, stdout } = require('node:process');
+const readline = require("node:readline/promises");
+const fs = require("fs/promises");
+const path = require("path");
+const { stdin, stdout } = require("node:process");
 const {
   Mnemonic,
   PrivateKey,
@@ -11,47 +11,47 @@ const {
   AccountCreateTransaction,
   Hbar,
   HbarUnit,
-} = require('@hashgraph/sdk');
-const dotenv = require('dotenv');
+} = require("@hashgraph/sdk");
+const dotenv = require("dotenv");
 const {
   createLogger,
   queryAccountByEvmAddress,
   queryAccountByPrivateKey,
   isHexPrivateKey,
   CHARS,
-} = require('../util/util.js');
+} = require("../util/util.js");
 
 let logger;
 let client;
 
 const DEFAULT_VALUES = {
-  dotEnvFilePath: path.resolve(__dirname, '../.env'),
-  rpcUrl: 'http://localhost:7546/',
+  dotEnvFilePath: path.resolve(__dirname, "../.env"),
+  rpcUrl: "http://localhost:7546/",
   numAccounts: 3,
   numAccountsMinimum: 1,
 };
 
 async function initDotEnvForApp() {
   logger = await createLogger({
-    scriptId: 'initDotEnvForApp',
-    scriptCategory: 'setup',
+    scriptId: "initDotEnvForApp",
+    scriptCategory: "setup",
   });
-  logger.logStart('Initialise .env file - start');
+  logger.logStart("Initialize .env file - start");
 
   // prompt for inputs
   const { allowOverwrite1stChar, dotEnvText, accounts } = await promptInputs();
 
   // write `.env` file if instructed
-  if (allowOverwrite1stChar === 'y') {
-    logger.log('OK, overwriting .env file');
+  if (allowOverwrite1stChar === "y") {
+    logger.log("OK, overwriting .env file");
     const fileName = DEFAULT_VALUES.dotEnvFilePath;
     await fs.writeFile(fileName, dotEnvText);
-    logger.log('Overwrite .env file');
+    logger.log("Overwrite .env file");
   } else {
-    logger.log('Leave as-is .env file');
+    logger.log("Leave as-is .env file");
   }
 
-  logger.logComplete('Initialise .env file - complete');
+  logger.logComplete("Initialize .env file - complete");
 }
 
 function constructDotEnvFile({
@@ -70,7 +70,7 @@ ACCOUNT_${accountIndex}_ID=${account.id}
 `;
       return text;
     })
-    .join('\n');
+    .join("\n");
   const output = `# This .env file stores credentials for Hedera Testnet only.
 # Do **not** reuse or share any credentials from Hedera Mainnet,
 # as this file is stored as plain text on disk,
@@ -103,16 +103,16 @@ async function getUsableAccount(privateKeyStr, evmAddress) {
   let { accountId, accountBalance, accountEvmAddress } = account;
   if (!accountId) {
     throw new Error(
-      'Must specify an account which exists, and can be derived from the specified ECDSA secp256k1 private key',
+      "Must specify an account which exists, and can be derived from the specified ECDSA secp256k1 private key"
     );
   }
   if (!accountBalance) {
-    throw new Error('Must specify an account which is funded');
+    throw new Error("Must specify an account which is funded");
   }
 
   return {
     privateKey: privateKeyStr,
-    evmAddress: accountEvmAddress || '',
+    evmAddress: accountEvmAddress || "",
     id: accountId,
   };
 }
@@ -129,8 +129,8 @@ async function promptInputs() {
   let numAccounts = NUM_ACCOUNTS;
   let accounts = [];
   let rpcUrl = RPC_URL;
-  let allowOverwrite1stChar = '';
-  let dotEnvText = '';
+  let allowOverwrite1stChar = "";
+  let dotEnvText = "";
 
   const rlPrompt = readline.createInterface({
     input: stdin,
@@ -144,14 +144,14 @@ async function promptInputs() {
   do {
     if (restart) {
       console.error(
-        "\n❌ Invalid input values detected, the '.env' file has not been initialised.",
+        "\n❌ Invalid input values detected, the '.env' file has not been initialized."
       );
-      logger.log('Restarting the interactive prompts', CHARS.HELLIP);
+      logger.log("Restarting the interactive prompts", CHARS.HELLIP);
     }
     logger.logSectionWithoutWaitPrompt(
-      'Please enter values requested, or accept defaults, in the interactive prompts below.',
+      "Please enter values requested, or accept defaults, in the interactive prompts below."
     );
-    logger.log("These will be used to initialise the '.env' file.\n");
+    logger.log("These will be used to initialize the '.env' file.\n");
 
     restart = false;
     let use1stAccountAsOperator = false;
@@ -159,14 +159,14 @@ async function promptInputs() {
     // prompt for BIP-39 seed phrase
     // - user may leave empty, and a new one will be generated at random
     // - user may specify a seed phrase, in which case it will be validated
-    logger.log('Enter a BIP-39 seed phrase');
+    logger.log("Enter a BIP-39 seed phrase");
     if (seedPhrase) {
       logger.log(`Current: "${seedPhrase}"`);
-      logger.log('(enter blank to re-use the above value)');
+      logger.log("(enter blank to re-use the above value)");
     } else {
-      logger.log('(enter blank value generate a new one at random)');
+      logger.log("(enter blank value generate a new one at random)");
     }
-    const inputSeedPhrase = await rlPrompt.question('> ');
+    const inputSeedPhrase = await rlPrompt.question("> ");
     seedPhrase = inputSeedPhrase || seedPhrase;
 
     // validate seed phrase or generate a new one
@@ -175,7 +175,7 @@ async function promptInputs() {
       // generate new seed phrase
       mnemonic = await Mnemonic.generate12();
       seedPhrase = mnemonic.toString();
-      logger.log('Randomly-generated seed phrase: ', seedPhrase);
+      logger.log("Randomly-generated seed phrase: ", seedPhrase);
     } else {
       // validate specified seed phrase
       let isValidatedSeedPhrase = true;
@@ -186,7 +186,7 @@ async function promptInputs() {
       }
       if (!isValidatedSeedPhrase) {
         await logger.logError(
-          'Specified input is not a valid BIP-39 seed phrase',
+          "Specified input is not a valid BIP-39 seed phrase"
         );
         restart = true;
         continue;
@@ -197,16 +197,16 @@ async function promptInputs() {
     // - the desired number of accounts will be generated (minimum  1)
     // - however these accounts will not yet have account IDs, until funded
     logger.log(
-      'Enter a number of accounts to generate from your BIP-39 seed phrase',
+      "Enter a number of accounts to generate from your BIP-39 seed phrase"
     );
     if (numAccounts) {
       logger.log(`Current: "${numAccounts}"`);
-      logger.log('(enter blank to re-use the above value)');
+      logger.log("(enter blank to re-use the above value)");
     } else {
       logger.log(`Default: "${DEFAULT_VALUES.numAccounts}"`);
-      logger.log('(enter blank value to use default value)');
+      logger.log("(enter blank value to use default value)");
     }
-    const inputNumAccounts = await rlPrompt.question('> ');
+    const inputNumAccounts = await rlPrompt.question("> ");
     numAccounts = inputNumAccounts || numAccounts || DEFAULT_VALUES.numAccounts;
     numAccounts = parseInt(numAccounts, 10);
     if (isNaN(numAccounts)) {
@@ -225,15 +225,15 @@ async function promptInputs() {
     for (let accountIndex = 0; accountIndex < numAccounts; ++accountIndex) {
       const accountPrivateKeyObj =
         await mnemonic.toStandardECDSAsecp256k1PrivateKeyCustomDerivationPath(
-          '',
-          `m/44'/60'/0'/0/${accountIndex}`,
+          "",
+          `m/44'/60'/0'/0/${accountIndex}`
         );
       const accountPrivateKey = `0x${accountPrivateKeyObj.toStringRaw()}`;
       const accountEvmAddress = `0x${accountPrivateKeyObj.publicKey.toEvmAddress()}`;
       accounts[accountIndex] = {
         privateKey: accountPrivateKey,
         evmAddress: accountEvmAddress,
-        id: '',
+        id: "",
       };
     }
 
@@ -241,15 +241,15 @@ async function promptInputs() {
     // - defaults to localhost
     // - on gitpod, `RPC_URL` env var is expected to be set prior to
     //   invoking this script
-    logger.log('Enter your preferred JSON-RPC endpoint URL');
+    logger.log("Enter your preferred JSON-RPC endpoint URL");
     if (rpcUrl) {
       logger.log(`Current: "${rpcUrl}"`);
-      logger.log('(enter blank to re-use the above value)');
+      logger.log("(enter blank to re-use the above value)");
     } else {
       logger.log(`Default: "${DEFAULT_VALUES.rpcUrl}"`);
-      logger.log('(enter blank value to use default value)');
+      logger.log("(enter blank value to use default value)");
     }
-    const inputRpcUrl = await rlPrompt.question('> ');
+    const inputRpcUrl = await rlPrompt.question("> ");
     if (!inputRpcUrl) {
       rpcUrl = rpcUrl || DEFAULT_VALUES.rpcUrl;
     } else {
@@ -262,28 +262,28 @@ async function promptInputs() {
     // - user may opt to specify an account private key
     // - if private key is specified, validation will be performed,
     //   and account ID will be obtained from there (no need to ask user to input)
-    logger.log('Enter your operator account private key');
+    logger.log("Enter your operator account private key");
     logger.log(
-      'Note that this must be an ECDSA secp256k1 private key, and hexadecimal encoded.',
+      "Note that this must be an ECDSA secp256k1 private key, and hexadecimal encoded."
     );
     if (operatorKey) {
       logger.log(`Current: "${operatorKey}"`);
-      logger.log('(enter blank to re-use the above value)');
+      logger.log("(enter blank to re-use the above value)");
       logger.log(
-        '(enter "seed" use first account from BIP-39 seed phrase as operator account)',
+        '(enter "seed" use first account from BIP-39 seed phrase as operator account)'
       );
     } else {
       logger.log(
-        'e.g. "0xabcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890"',
+        'e.g. "0xabcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890"'
       );
       logger.log(
-        '(enter blank to use first account from BIP-39 seed phrase as operator account)',
+        "(enter blank to use first account from BIP-39 seed phrase as operator account)"
       );
     }
-    const inputOperatorKey = (await rlPrompt.question('> ')).trim();
+    const inputOperatorKey = (await rlPrompt.question("> ")).trim();
     if (
-      (!operatorKey && inputOperatorKey === '') ||
-      inputOperatorKey === 'seed'
+      (!operatorKey && inputOperatorKey === "") ||
+      inputOperatorKey === "seed"
     ) {
       use1stAccountAsOperator = true;
       operatorKey = accounts[0].privateKey;
@@ -291,7 +291,7 @@ async function promptInputs() {
       operatorKey = inputOperatorKey || operatorKey;
     }
     if (!operatorKey) {
-      await logger.logError('Must specify operator account private key');
+      await logger.logError("Must specify operator account private key");
       restart = true;
       continue;
     }
@@ -304,7 +304,7 @@ async function promptInputs() {
     // A different public key and therefore EVM address will be generated,
     // and when detecting if that account has been funded, will then fail.
     if (!isHexPrivateKey(operatorKey)) {
-      await logger.logError('Must use operator key of hexadecimal format');
+      await logger.logError("Must use operator key of hexadecimal format");
       restart = true;
       continue;
     }
@@ -312,28 +312,28 @@ async function promptInputs() {
     const operatorAccountPublicKey =
       PrivateKey.fromStringECDSA(operatorKey).publicKey;
     console.log(
-      'operatorAccountPublicKey',
-      operatorAccountPublicKey.toStringRaw(),
+      "operatorAccountPublicKey",
+      operatorAccountPublicKey.toStringRaw()
     );
     const operatorAccountEvmAddress =
-      '0x' + operatorAccountPublicKey.toEvmAddress();
+      "0x" + operatorAccountPublicKey.toEvmAddress();
 
     // first, give user the opportunity to fund this account
     logger.log(
-      `Please ensure that you have funded ${operatorAccountEvmAddress}`,
+      `Please ensure that you have funded ${operatorAccountEvmAddress}`
     );
     logger.log(
-      'If this account has not yet been created or funded, you may do so via',
-      ...logger.applyAnsi('URL', 'https://faucet.hedera.com'),
+      "If this account has not yet been created or funded, you may do so via",
+      ...logger.applyAnsi("URL", "https://faucet.hedera.com")
     );
-    logger.log('(Simply enter a blank value to when this account is ready)');
-    await rlPrompt.question('> '); // discard the response, no use for it
+    logger.log("(Simply enter a blank value to when this account is ready)");
+    await rlPrompt.question("> "); // discard the response, no use for it
 
     // validate operator account details
     try {
       operatorAccount = await getUsableAccount(
         operatorKey,
-        operatorAccountEvmAddress,
+        operatorAccountEvmAddress
       );
     } catch (ex) {
       // Fail fast here, as we know this account is non-functional in its present state
@@ -346,7 +346,7 @@ async function promptInputs() {
       accounts[0] = operatorAccount;
     }
 
-    logger.logSectionWithoutWaitPrompt('Checking all accounts');
+    logger.logSectionWithoutWaitPrompt("Checking all accounts");
     for (let idx = 0; idx < numAccounts; idx++) {
       const account = accounts[idx];
 
@@ -362,13 +362,13 @@ async function promptInputs() {
       logger.log(`Account #${idx} does not exist, creating`, CHARS.HELLIP);
 
       const operatorPrivateKeyObj = PrivateKey.fromStringECDSA(
-        operatorAccount.privateKey,
+        operatorAccount.privateKey
       );
       if (!client) {
         // lazily instantiate a client instance when first necessary
         client = Client.forTestnet().setOperator(
           operatorAccount.id,
-          operatorPrivateKeyObj,
+          operatorPrivateKeyObj
         );
       }
 
@@ -379,12 +379,14 @@ async function promptInputs() {
         .freezeWith(client);
 
       const accountCreateTxSigned = await accountCreateTx.sign(
-        operatorPrivateKeyObj,
+        operatorPrivateKeyObj
       );
-      const accountCreateTxSubmitted =
-        await accountCreateTxSigned.execute(client);
-      const accountCreateTxReceipt =
-        await accountCreateTxSubmitted.getReceipt(client);
+      const accountCreateTxSubmitted = await accountCreateTxSigned.execute(
+        client
+      );
+      const accountCreateTxReceipt = await accountCreateTxSubmitted.getReceipt(
+        client
+      );
       account.id = accountCreateTxReceipt.accountId.toString();
 
       logger.log(`Account #${idx} created:`, account.id);
@@ -404,12 +406,12 @@ async function promptInputs() {
       rpcUrl,
     });
     logger.log(dotEnvText);
-    logger.log('Do you wish to overwrite the .env file with the above?');
-    logger.log('(restart/yes/No)');
-    const inputAllowOverwrite = await rlPrompt.question('> ');
+    logger.log("Do you wish to overwrite the .env file with the above?");
+    logger.log("(restart/yes/No)");
+    const inputAllowOverwrite = await rlPrompt.question("> ");
     allowOverwrite1stChar = inputAllowOverwrite.toLowerCase().charAt(0);
-    if (allowOverwrite1stChar === 'r') {
-      logger.log('OK, restarting...');
+    if (allowOverwrite1stChar === "r") {
+      logger.log("OK, restarting...");
       restart = true;
     }
   } while (restart);
